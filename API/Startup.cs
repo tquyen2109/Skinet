240017ad7aa3141ac622457b75a51d4705d86bb6
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-03 18:22:35 -0400

    Clean up Startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,23 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
-using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
 using API.MiddleWare;
-using API.Errors;
-using Microsoft.OpenApi.Models;
+using API.Extension;
 
 namespace API
 {
@@ -37,27 +27,9 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlServer
            (_configuration.GetConnectionString("DefaultConnection")));
-           services.AddScoped<IProductRepository,ProductRepository>();
-           services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
            services.AddAutoMapper(typeof(MappingProfiles));
-           services.Configure<ApiBehaviorOptions>(options => {
-               options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                   var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(errorResponse);
-               };
-           });
-           services.AddSwaggerGen(c => 
-           {
-               c.SwaggerDoc("v1", new OpenApiInfo {Title = "Skinet API", Version = "v1"});
-           });
+           services.AddApplicationServices();
+           services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,11 +43,7 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => 
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Skinet API v1");
-            });
+            app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
