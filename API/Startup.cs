e1c24f56afec387750ebc27617a36707b86601bb
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-03 18:08:13 -0400

    Error Handling API

--- API/Startup.cs
+++ API/Startup.cs
@@ -15,6 +15,9 @@ using Infrastructure.Data;
 using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.MiddleWare;
+using API.Errors;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -37,23 +40,42 @@ namespace API
            services.AddScoped<IProductRepository,ProductRepository>();
            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
            services.AddAutoMapper(typeof(MappingProfiles));
+           services.Configure<ApiBehaviorOptions>(options => {
+               options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                   var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+               };
+           });
+           services.AddSwaggerGen(c => 
+           {
+               c.SwaggerDoc("v1", new OpenApiInfo {Title = "Skinet API", Version = "v1"});
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseStaticFiles();
 
             app.UseAuthorization();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Skinet API v1");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
