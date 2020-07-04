9e37b901fb010f0017879206c73a583d8e89ce80
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-03 20:49:30 -0400

    Add Filter for Brand and Type API

--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -11,7 +11,11 @@ namespace Core.Specification
             AddInclude(x => x.ProductBrand);
         }
         
-        public ProductsWithTypesAndBrandsSpecification(string sort)
+        public ProductsWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId) 
+            : base(x => 
+                (!brandId.HasValue || x.ProductBrandId == brandId) &&
+                (!typeId.HasValue || x.ProductTypeId == typeId)
+            )
         {
             AddOrderBy(x => x.Name);
             AddInclude(x => x.ProductType);
