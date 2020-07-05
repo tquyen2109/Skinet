d9eece6f18c706e34a9b2ae6f0cddcb3011dd4b5
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-05 09:38:38 -0400

    Product Search by name

--- Core/Specification/ProductWithFiltersForCountSpecification.cs
+++ Core/Specification/ProductWithFiltersForCountSpecification.cs
@@ -6,6 +6,7 @@ namespace Core.Specification
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productSpecParams)
          : base(x => 
+                (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search)) &&
                 (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId) &&
                 (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId)
             )
--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -13,6 +13,7 @@ namespace Core.Specification
         
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productSpecParams) 
             : base(x => 
+                (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search)) &&
                 (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId) &&
                 (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId)
             )
