fe024886b646195712a9eead1bf2bacd6cfaf3b5
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-27 18:15:28 -0400

    Change basket type

--- Core/Entity/BasketItem.cs
+++ Core/Entity/BasketItem.cs
@@ -8,6 +8,6 @@ namespace Core.Entity
         public int Quantity { get; set; }
         public string PictureUrl { get; set; }
         public string Brand { get; set; }
-        public string Types { get; set; }
+        public string Type { get; set; }
     }
 }
