bb59114c280df9b7c547a552ecf4d5959ceed79f
Quyen Tran <t.quyen1997@yahoo.com>
2020-07-27 21:59:57 -0400

    Product-detail add to basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './../../basket/basket.service';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from './../shop.service';
 import { IProduct } from './../../shared/models/product';
@@ -11,12 +12,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+  quantity = 1;
+  constructor(private shopService: ShopService,
+              private activateRoute: ActivatedRoute,
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
     this.bcService.set('@productDetails', '');
   }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1){
+      this.quantity--;
+    }
+      
   }
 
   loadProduct() {
@@ -25,6 +45,6 @@ export class ProductDetailsComponent implements OnInit {
       this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
-    })
+    });
   }
 }
